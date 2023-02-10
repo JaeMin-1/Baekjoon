1bab1ee78b654b31b871510b2d1dd929767e64c8
JaeMin <39516729+JaeMin-1@users.noreply.github.com>
2023-02-10 22:07:34 +0900

    [Bronze V] Title: 검증수, Time: 124 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int sum = 0;
+        for(int i=0; i<5; i++) {
+            sum += Math.pow(Integer.parseInt(st.nextToken()), 2);
+        }
+        int ans = sum % 10;
+        System.out.println(ans);
+    }
+}
