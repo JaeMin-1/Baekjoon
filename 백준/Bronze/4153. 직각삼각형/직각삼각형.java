44ae667c7b0ce313184ed521dbf4efd22afef374
JaeMin <39516729+JaeMin-1@users.noreply.github.com>
2023-02-12 00:56:50 +0900

    [Bronze III] Title: 직각삼각형, Time: 116 ms, Memory: 13964 KB -BaekjoonHub

--- 백준/Bronze/4153. 직각삼각형/직각삼각형.java
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.java
@@ -1,32 +1,34 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
+import java.io.IOException;
 public class Main {
-	public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-       
         while(true) {
-        	StringTokenizer st = new StringTokenizer(br.readLine()," ");
-            
-        	int x = Integer.parseInt(st.nextToken());
-        	int y = Integer.parseInt(st.nextToken());
-        	int z = Integer.parseInt(st.nextToken());
-        	
-        	if(x == 0 && y == 0 && z == 0) break;
-            
-        	if((x * x + y * y) == z * z) {
-				System.out.println("right");
-			}
-        	else if(x * x == (y * y + z * z)) {
-				System.out.println("right");
-			}
-        	else if(y * y == (z * z + x * x)) {
-				System.out.println("right");
-			}
-        	else {
-				System.out.println("wrong");
-			}
-		}
-	}
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            int z = Integer.parseInt(st.nextToken());
+            if(x == 0 && y == 0 && z == 0) break;
+            else if(x>=y && x>=z) {
+                if(x*x == y*y + z*z) {
+                    System.out.println("right");
+                }
+                else System.out.println("wrong");
+            }
+            else if(y>=x && y>=z) {
+                if(y*y == x*x + z*z) {
+                    System.out.println("right");
+                }
+                else System.out.println("wrong");
+            }
+            else if(z>=x && z>=y) {
+                if(z*z == x*x + y*y) {
+                    System.out.println("right");
+                }
+                else System.out.println("wrong");
+            }
+        }
+    }
 }
