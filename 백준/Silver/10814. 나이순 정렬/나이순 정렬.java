efee11285dfadaf26ac356bbaf5cff2d3a1c0a18
JaeMin <39516729+JaeMin-1@users.noreply.github.com>
2023-02-18 16:34:45 +0900

    [Silver V] Title: 나이순 정렬, Time: 716 ms, Memory: 50180 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.IOException;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        String[][] arr = new String[N][2];
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = st.nextToken();
+            arr[i][1] = st.nextToken();
+        }
+
+        Arrays.sort(arr, (e1, e2) -> {   
+            return Integer.parseInt(e1[0]) - Integer.parseInt(e2[0]);
+        });
+
+        for(int i=0; i<N; i++) {
+            sb.append(arr[i][0]).append(" ").append(arr[i][1]).append('\n');
+        }
+        System.out.println(sb);
+    }
+}
