196f1fc032ba35d9fd9b93b8f81a5b2c11530548
JaeMin <39516729+JaeMin-1@users.noreply.github.com>
2023-03-01 17:39:53 +0900

    [Silver IV] Title: 설탕 배달, Time: 124 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -5,19 +5,9 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
         int N = Integer.parseInt(br.readLine());
 
-        int cnt = 0;
-        while(N>=0) {
-            if(N % 5 == 0) {
-                cnt += N / 5;
-                System.out.println(cnt);
-                return;
-            }
-            
-            else {
-                N -= 3;
-                cnt++;
-            }
-        }
-        System.out.println(-1);
+        if (N == 4 || N == 7) System.out.println(-1);
+        else if(N%5 == 0) System.out.println(N/5);
+        else if (N%5 == 1 || N%5 == 3) System.out.println(N/5 + 1);
+        else if (N%5 == 2 || N%5 == 4) System.out.println(N/5 + 2);
     }
 }
