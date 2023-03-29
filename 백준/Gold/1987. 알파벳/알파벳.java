01b39a91957c54f672238c0dc620f5c9bd95e756
JaeMin <39516729+JaeMin-1@users.noreply.github.com>
2023-03-29 11:54:02 +0900

    [Gold IV] Title: 알파벳, Time: 856 ms, Memory: 15280 KB -BaekjoonHub

---
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int r, c;
+    static char[][] map;
+    static boolean[] visited = new boolean[26];
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {1, 0, -1, 0};
+    static int ans = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        r = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+
+        map = new char[r][c];
+        for (int i = 0; i < r; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < c; j++) {
+                map[i][j] = str.charAt(j);
+            }
+        }
+
+        dfs(0, 0, 1);
+
+        System.out.println(ans);
+    }
+
+    public static void dfs(int x, int y, int cnt) {
+        ans = Math.max(ans, cnt);
+
+        visited[map[x][y] - 'A'] = true;
+
+        for (int i=0; i<4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx < 0 || nx >= r || ny < 0 || ny >= c) {
+                continue;
+            }
+
+            if (!visited[map[nx][ny] - 'A']) {
+                dfs(nx, ny, cnt + 1);
+            }
+        }
+
+        visited[map[x][y] - 'A'] = false;
+    }
+}
