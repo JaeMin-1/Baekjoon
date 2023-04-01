3fc636509d827a0a7d6241a664f7fd82d3be37ae
JaeMin <39516729+JaeMin-1@users.noreply.github.com>
2023-04-01 12:57:43 +0900

    [Bronze I] Title: 약수, Time: 128 ms, Memory: 14304 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int result = arr[0] * arr[n-1]; // 최솟값과 최댓값을 곱해서 결과 도출
+        System.out.println(result);
+    }
+}
