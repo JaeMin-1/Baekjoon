7e4ca1be664ede3ac43c66a0244e5e5361ccc598
JaeMin <39516729+JaeMin-1@users.noreply.github.com>
2023-04-24 21:01:44 +0900

    [Bronze V] Title: 웰컴, Time: 116 ms, Memory: 13956 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(".  .   .\n");
+        sb.append("|  | _ | _. _ ._ _  _\n");
+        sb.append("|/\\|(/.|(_.(_)[ | )(/.\n");
+
+        System.out.println(sb);
+    }
+}
