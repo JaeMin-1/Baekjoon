f396ec27491d8fcdeadefef6c686cbb952e2ea3b
JaeMin <39516729+JaeMin-1@users.noreply.github.com>
2023-05-01 21:01:37 +0900

    [Bronze V] Title: 영수증, Time: 132 ms, Memory: 14060 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int X = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+
+        while(N-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            X -= a * b;
+        }
+
+        if(X== 0) System.out.println("Yes");
+        else System.out.println("No");
+
+    }
+}
