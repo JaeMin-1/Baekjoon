2671de865677ce1edc3d5c46c6f52437df5fe982
JaeMin <39516729+JaeMin-1@users.noreply.github.com>
2023-08-05 23:25:11 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 55.08 ms, Memory: 116 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -2,17 +2,19 @@ import java.util.*;
 
 public class Solution {
     public int[] solution(int []arr) {
-        ArrayList<Integer> tempList = new ArrayList<>();
-        int preNum = 10;
-        for(int num : arr) {
-            if(preNum != num)
-                tempList.add(num);
-            preNum = num;
-        }       
-        int[] answer = new int[tempList.size()];
-        for(int i=0; i<answer.length; i++) {
-            answer[i] = tempList.get(i);
+        Stack<Integer> stack = new Stack<>();
+        for(int i : arr) {
+            if(stack.isEmpty()) stack.push(i);
+            else {
+                if(stack.peek() != i) stack.push(i);
+            }
         }
+        int[] answer = new int[stack.size()];
+        for(int i=stack.size()-1; i>=0; i--) {
+            answer[i] = stack.pop();
+        }
+
+
         return answer;
     }
 }
