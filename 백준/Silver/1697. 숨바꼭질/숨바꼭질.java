1d77cdd88579a348e56957a5656ae0253d9d802e
JaeMin <39516729+JaeMin-1@users.noreply.github.com>
2023-09-13 01:44:21 +0900

    [Silver I] Title: 숨바꼭질, Time: 172 ms, Memory: 23952 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -6,7 +6,7 @@ public class Main {
     static int[] ch = new int[100001];
     public static int BFS(int n, int k) {
         if(n==k) return 0;
-
+        
         Q.offer(n);
         ch[n] = 1;
         int L = 0;
@@ -16,13 +16,11 @@ public class Main {
                 int x = Q.poll();
                 int[] move = {-1, 1, x};
                 for(int j=0; j<3; j++) {
-                    if(x+move[j] >=0 && x+move[j]<=100000) {
-                        int nx = x + move[j];
+                    int nx = x + move[j];
+                    if(nx>=0 && nx<=100000 && ch[nx] == 0) {
                         if(nx==k) return L+1;
-                        if(nx>=0 && nx<=100000 && ch[nx] == 0) {
-                            Q.offer(nx);
-                            ch[nx] = 1;
-                        }
+                        Q.offer(nx);
+                        ch[nx] = 1;
                     }
                 }
             }
