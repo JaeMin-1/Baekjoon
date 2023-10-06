c6f2b5832cb447547fd16062fbfbcbe15d34aa79
JaeMin <39516729+JaeMin-1@users.noreply.github.com>
2023-10-06 17:47:02 +0900

    [D3] Title: [S/W 문제해결 응용] 2일차 - 최대 상금, Time: 553 ms, Memory: 20,972 KB -BaekjoonHub

---
+++ SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    static int answer;
+    public static void DFS(Integer[] arr, int cnt) {
+        if(cnt == 0) {
+            int sum = 0, num = 1;
+            for(int i = arr.length-1; i>=0; i--) {
+                sum += arr[i] * num;
+                num *= 10;
+            }
+            answer = Math.max(answer, sum);
+            return;
+
+        }
+        else {
+            for(int i=0; i<arr.length-1; i++) {
+                for(int j=i+1; j<arr.length; j++) {
+                    int tmp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = tmp;
+                    DFS(arr, cnt-1);
+                    tmp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = tmp;
+                }
+            }
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String[] str = st.nextToken().split("");
+            Integer[] arr = new Integer[str.length];
+            for(int i=0; i<str.length; i++) {
+                arr[i] = Integer.parseInt(str[i]);
+            }
+
+            int cnt = Integer.parseInt(st.nextToken());
+            if(arr.length<cnt) cnt = arr.length;
+            answer = 0;
+            DFS(arr, cnt);
+
+            sb.append("#").append(test_case).append(" ").append(answer).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
