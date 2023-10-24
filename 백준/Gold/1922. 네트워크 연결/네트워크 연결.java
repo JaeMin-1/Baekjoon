f0fa69c2c809922b7e5060f45a911b63fda33268
JaeMin <39516729+JaeMin-1@users.noreply.github.com>
2023-10-25 03:11:37 +0900

    [Gold IV] Title: 네트워크 연결, Time: 592 ms, Memory: 50336 KB -BaekjoonHub

---
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.*;
+
+class Computer {
+    int v1, v2, cost;
+    Computer(int v1, int v2, int cost) {
+        this.v1 = v1;
+        this.v2 = v2;
+        this.cost = cost;
+    }
+}
+
+public class Main {
+    static int answer;
+    static ArrayList<Computer> arr;
+    static int n, m;
+    static int[] unf;
+    public static int Find(int v) {
+        if(v == unf[v]) return v;
+        else return unf[v] = Find(unf[v]);
+    }
+    public static void Union(int a, int b) {
+        int fa = Find(a);
+        int fb = Find(b);
+        if(fa < fb) unf[fb] = fa;
+        else unf[fa] = fb;
+    }
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        m = Integer.parseInt(br.readLine());
+        unf = new int[n+1];
+        for(int i=1; i<=n; i++) unf[i] = i;
+        arr = new ArrayList<>();
+        for(int i=0; i<m; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            arr.add(new Computer(a, b, c));
+        }
+        Collections.sort(arr, (o1, o2) -> {
+            return o1.cost - o2.cost;
+        });
+        for(Computer ob : arr) {
+            int fa = Find(ob.v1);
+            int fb = Find(ob.v2);
+            if(fa!=fb) {
+                answer += ob.cost;
+                Union(ob.v1, ob.v2);
+            }
+        }
+        System.out.println(answer);
+    }
+}
